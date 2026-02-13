ca9a5a852fbc09ade79de41ef7b8af540c8004f2
yingxingsilver <155343857+yingxingsilver@users.noreply.github.com>
2026-02-13 10:57:45 +0800

    2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,61 @@
-repositories {
-  maven {
-    name = "papermc"
-    url = uri("https://repo.papermc.io/repository/maven-public/")
-  }
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
+
+plugins {
+  `java-library`
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+  // id("xyz.jpenilla.run-paper") version "3.0.2" // Adds runServer and runMojangMappedServer tasks for testing
+  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.3.0" // Generates plugin.yml based on the Gradle config
 }
 
-dependencies {
-  compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-}
+group = "top.rainmc.testplugin"
+version = "1.0.0-SNAPSHOT"
+description = "Test plugin for paperweight-userdev"
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+  // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
+  toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
-tasks.jar {
-  manifest {
-    attributes["paperweight-mappings-namespace"] = "mojang"
-  }
+// For 1.20.4 or below, or when you care about supporting Spigot on >=1.20.5:
+/*
+paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.REOBF_PRODUCTION
+
+tasks.assemble {
+  dependsOn(tasks.reobfJar)
 }
-// if you have shadowJar configured
-tasks.shadowJar {
-  manifest {
-    attributes["paperweight-mappings-namespace"] = "mojang"
+ */
+
+dependencies {
+  paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
+  // paperweight.foliaDevBundle("1.21.10-R0.1-SNAPSHOT")
+  // paperweight.devBundle("com.example.paperfork", "1.21.10-R0.1-SNAPSHOT")
+}
+
+tasks {
+  compileJava {
+    // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+    // See https://openjdk.java.net/jeps/247 for more information.
+    options.release = 21
   }
+  javadoc {
+    options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+  }
+
+  // Only relevant for 1.20.4 or below, or when you care about supporting Spigot on >=1.20.5:
+  /*
+  reobfJar {
+    // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
+    // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
+    outputJar = layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar")
+  }
+   */
+}
+
+// Configure plugin.yml generation
+// - name, version, and description are inherited from the Gradle project.
+bukkitPluginYaml {
+  main = "top.rainmc.testplugin.ExamplePlugin"
+  load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+  authors.add("Author")
+  apiVersion = "1.21.10"
 }
